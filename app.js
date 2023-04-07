b727a972bec9598e78235da201f05aaeb25c9023
Ferhad <qayibovferhad04@gmail.com>
2023-04-07 14:19:05 +0400

    updated

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const helmet = require("helmet");
 const blogRoutes = require("./routes/blogRoutes");
 const userRoutes = require("./routes/userRoutes");
 const errorMiddleware = require("./middleware/errorMiddleware");
-const app = express();
 const path = require("path");
+const app = express();
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 
 mongoose.connect(CONNECTION_STRING);
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,7 @@
-const express = require("express");
 const blogRouter = express.Router();
-const blogController = require("../controllers/blog");
+const express = require("express");
 const authMiddleware = require("../middleware/auth");
+const blogController = require("../controllers/blog");
 
 blogRouter.use(authMiddleware);
 blogRouter.get("/blogs", blogController.getBlogs);
