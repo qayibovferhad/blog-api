3bb2c2e8bdde6a0fd71ab8b7023ccfd59ea97376
Ferhad <qayibovferhad04@gmail.com>
2023-04-13 22:14:55 +0400

    updated

--- middleware/fileUpload.js
+++ middleware/fileUpload.js
@@ -1,5 +1,4 @@
 const multer = require("multer");
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/uploads");
@@ -7,7 +6,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const filePartName = file.originalname.split(".");
     const exc = filePartName[filePartName.length - 1];
-    const uniqiue = Date.now() + "-" + Math.random(Math.random() * 10000);
+    const uniqiue = Date.now() + "-" + Math.round(Math.random() * 10000);
     cb(null, uniqiue + "." + exc);
   },
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const PasswordReset = require("../models/passwordReset");
 const userRouter = express.Router();
-const crypto = require("crypto");
 const userController = require("../controllers/user");
 const upload = require("../middleware/fileUpload");
 const SALT = process.env.PASSWORD_SALT;
